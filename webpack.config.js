ebf49075f4d944938dfcdde8b198d108afc4adf7
Bendik Solheim <hello@bendik.dev>
2020-02-14 21:34:16 +0100

    Use webpack instead of parcel

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const isDev = mode => mode === "development";
+
+module.exports = (env, { mode } = { mode: "development" }) => ({
+  entry: path.resolve(__dirname, "src", "index.ts"),
+  output: {
+    filename: "[name].[hash].js",
+    path: path.resolve(__dirname, "docs"),
+    chunkFilename: "[name].[hash].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        include: [path.resolve(__dirname, "src")],
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["css-loader"]
+      },
+      {
+        test: /\.(png)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]"
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".ts"],
+    mainFiles: ["index"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "index.html")
+    })
+  ],
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        // disable default chunks
+        default: false,
+        vendors: false,
+
+        // put modules required from node_modules or vendor folders in vendor chunk
+        vendor: {
+          test: /node_modules/,
+          chunks: "all"
+        }
+      }
+    }
+  },
+
+  devtool: "cheap-eval-source-map",
+
+  devServer: {
+    compress: true,
+    port: 1234
+  }
+});
