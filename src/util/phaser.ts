a09f36b260bb30e7e42a6c490afdc17790264d01
Bendik Solheim <hello@bendik.dev>
2020-02-21 21:59:45 +0100

    Rename object layer

--- src/util/phaser.ts
+++ src/util/phaser.ts
@@ -2,5 +2,5 @@ export function findObject(
   map: Phaser.Tilemaps.Tilemap,
   name: string
 ): Phaser.GameObjects.GameObject {
-  return map.findObject("routes", obj => obj.name === name);
+  return map.findObject("objects", obj => obj.name === name);
 }
