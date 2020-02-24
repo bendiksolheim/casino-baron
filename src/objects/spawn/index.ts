2af8486cd5f5507dbf04f81b473b1fe419c10d2e
Bendik Solheim <hello@bendik.dev>
2020-02-24 22:43:58 +0100

    Move getPath into phaser.ts file

--- src/objects/spawn/index.ts
+++ src/objects/spawn/index.ts
@@ -1,6 +1,6 @@
 import Phaser from "phaser";
 import Car from "../car";
-import { getPath } from "../../util/tiled";
+import { getPath } from "../../util/phaser";
 import { random, randomAround } from "../../util/random";
 import { findObject } from "../../util/phaser";
 import { Enter, Parked, Exit } from "../car/states";
--- src/util/phaser.ts
+++ src/util/phaser.ts
@@ -1,3 +1,5 @@
+import Phaser from "phaser";
+
 export type GameObjectWithLocation = Phaser.GameObjects.GameObject & {
   x: number;
   y: number;
@@ -11,4 +13,39 @@ export function findObject(
     "objects",
     obj => obj.name === name
   ) as GameObjectWithLocation;
+}
+
+type XYCoordT = {
+  x: number;
+  y: number;
+};
+
+type PolylineGameObjectT = Phaser.GameObjects.GameObject & {
+  polyline: XYCoordT[];
+  x: number;
+  y: number;
+};
+
+export function getPath(
+  scene: Phaser.Scene,
+  gameObject: Phaser.GameObjects.GameObject
+): Phaser.Curves.Path {
+  if (isPolyline(gameObject)) {
+    const { x, y, polyline } = gameObject as PolylineGameObjectT;
+    const path = scene.add.path(x, y);
+    polyline.forEach((coord: XYCoordT) => {
+      path.lineTo(x + coord.x, y + coord.y);
+    });
+    return path;
+  } else {
+    throw Error(`Object is not a polyline:\n${JSON.stringify(gameObject)}`);
+  }
+}
+
+function isPolyline(gameObject: Phaser.GameObjects.GameObject): boolean {
+  return (
+    gameObject.hasOwnProperty("polyline") &&
+    gameObject.hasOwnProperty("x") &&
+    gameObject.hasOwnProperty("y")
+  );
 }
