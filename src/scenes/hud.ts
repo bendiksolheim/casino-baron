68a24239b107b382e7390d9d6718ace1f27b2e75
Bendik Solheim <hello@bendik.dev>
2020-02-25 20:58:24 +0100

    Add hud, and move balance to it

--- src/scenes/hud.ts
+++ src/scenes/hud.ts
@@ -2,6 +2,7 @@ import Phaser from "phaser";
 import GameState, { ListenerT, GameStateT } from "../game-state";
 import tilemapResource from "../static/outdoor.json";
 import { GameObjectWithLocation, findObject } from "../util/phaser";
+import { decimalGroup } from "../util/number";
 
 const config: Phaser.Types.Scenes.SettingsConfig = {
   active: true,
@@ -25,17 +26,31 @@ export default class Hud extends Phaser.Scene implements ListenerT {
   create() {
     const map = this.make.tilemap({ key: "map" });
     this.cashPoint = findObject(map, "cash_point");
-    this.balanceText = this.add.text(10, 10, `${GameState.get().balance}$`, {
-      fontFamily: "Alphabeta",
-      fontSize: "32px",
-      fill: "#000"
-    });
+    const { width, height } = this.game.canvas;
+    this.rexUI;
+
+    this.rexUI.add
+      .roundRectangle(0, height, width, 40, 0, 0xcccccc)
+      .setOrigin(0.0, 1.0);
+
+    this.balanceText = this.add
+      .text(
+        width - 10,
+        height - 10,
+        `${decimalGroup(GameState.get().balance)}$`,
+        {
+          fontFamily: "Alphabeta",
+          fontSize: "24px",
+          fill: "#000"
+        }
+      )
+      .setOrigin(1.0, 1.0);
   }
 
   update() {}
 
   stateChanged(newState: GameStateT) {
-    this.balanceText.setText(`${GameState.get().balance}`);
+    this.balanceText.setText(`${decimalGroup(newState.balance)}$`);
     const text = this.add
       .text(this.cashPoint.x, this.cashPoint.y, `$`, {
         fontFamily: "Alphabeta",
