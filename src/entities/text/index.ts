a9eea8d2ed4dc6d77aecd254c29e56ce58bf157a
Bendik Solheim <hello@bendik.dev>
2020-03-06 09:46:24 +0100

    Component wrapping Phaser Text for keeping value

---
+++ src/entities/text/index.ts
@@ -0,0 +1,47 @@
+import Phaser from "phaser";
+
+const textStyle = {
+  fontFamily: "Alphabeta",
+  fontSize: "24px",
+  fill: "#000"
+};
+
+class Text<T> {
+  private text: Phaser.GameObjects.Text;
+  private value: T;
+  private transformer: (v: T) => string;
+
+  constructor(
+    scene: Phaser.Scene,
+    x: number,
+    y: number,
+    value: T,
+    transformer: (v: T) => string
+  ) {
+    this.value = value;
+    this.transformer = transformer;
+    this.text = scene.add
+      .text(x, y, transformer(value), textStyle)
+      .setOrigin(1.0, 1.0);
+  }
+
+  transformText(
+    textTransformer: (text: Phaser.GameObjects.Text) => void
+  ): Text<T> {
+    textTransformer(this.text);
+    return this;
+  }
+
+  setValue(value: T): boolean {
+    console.log(value, this.value);
+    if (value !== this.value) {
+      this.value = value;
+      this.text.setText(this.transformer(value));
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+export default Text;
