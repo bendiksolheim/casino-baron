e095ae52242ec8c6e5216040ea20b129bace293a
Bendik Solheim <hello@bendik.dev>
2020-03-28 21:03:02 +0100

    Moooore typings

--- src/typings/phaser-extensions.d.ts
+++ src/typings/phaser-extensions.d.ts
@@ -1,6 +1,27 @@
 import * as Phaser from "phaser";
 
 declare class RoundRectangle extends Phaser.GameObjects.Shape {}
+declare class Buttons extends Phaser.GameObjects.Shape {}
+declare class Label extends Phaser.GameObjects.Shape {}
+
+type ButtonConfig = {
+  x: number;
+  y: number;
+  width?: number;
+  height?: number;
+  buttons: Label[];
+  expand?: boolean;
+};
+
+type LabelConfig = {
+  x?: number;
+  y?: number;
+  width?: number;
+  height?: number;
+  text: Phaser.GameObjects.Text;
+  background: RoundRectangle;
+  align?: "left" | "top" | "center" | "right" | "bottom";
+};
 
 declare module "phaser" {
   interface Scene {
@@ -14,6 +35,10 @@ declare module "phaser" {
           radius: number,
           fillColor: number
         ) => RoundRectangle;
+
+        label: (config: LabelConfig) => Label;
+
+        buttons: (config: ButtonConfig) => Buttons;
       };
     };
   }
